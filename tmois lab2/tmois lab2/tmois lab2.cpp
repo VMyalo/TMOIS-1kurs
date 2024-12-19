4b141fd010e27a64ddd4ff379c41f4d3766bd70d
VMyalo <vsevolod.myalo@gmail.com>
2024-12-19 15:03:51 +0300

    Finished labs

---
+++ tmois lab2/tmois lab2/tmois lab2.cpp
@@ -0,0 +1,323 @@
+﻿// tmois lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <locale>
+using namespace std;
+
+void gr_output(vector<vector<int>> a)
+{
+    cout << "{" << endl;
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << "<";
+
+        for (int j = 0; j < 2; j++)
+        {
+            cout << a[i][j];
+            if (j == 0) cout << ", ";
+            if (j == 1) cout << ">" << endl;
+        }
+    }
+    cout << "}" << endl;
+    cout << "-----------------------------------------------------------------------" << endl;
+}
+
+void proj_output(vector<int> a)
+{
+    cout << "{";
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i];
+        if (i != a.size() - 1)
+            cout << ", ";
+    }
+    cout << "}";
+    cout << endl;
+
+    cout << "-----------------------------------------------------------------------" << endl;
+}
+
+vector<vector<int>> sort(vector<vector<int>> a)
+{
+    vector<vector<int>> result;
+    int contain = 0;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        for (int j = 0; j < result.size(); j++)
+        {
+            if (a[i][0] == result[j][0] && a[i][1] == result[j][1])
+                contain++;
+        }
+
+        if (contain == 0)
+            result.push_back({ a[i][0], a[i][1] });
+
+        contain = 0;
+    }
+
+    return result;
+}
+
+vector<int> sort2(vector<int> a)
+{
+    vector<int> result;
+    int contain = 0;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        for (int j = 0; j < result.size(); j++)
+        {
+            if (a[i] == result[j])
+                contain++;
+        }
+
+        if (contain == 0)
+            result.push_back(a[i]);
+
+        contain = 0;
+    }
+
+    return result;
+}
+
+vector<int> proj_on_axis(vector<vector<int>> a, int axis)
+{
+    vector<int> result;
+    for (int i = 0; i < a.size(); i++)
+    {
+        result.push_back(a[i][axis - 1]);
+    }
+
+    return sort2(result);
+}
+
+vector<vector<int>> inversion(vector<vector<int>> a)
+{
+    vector<vector<int>> result;
+    result = a;
+    for (int i = 0; i < a.size(); i++)
+    {
+        result[i][1] = a[i][0];
+        result[i][0] = a[i][1];
+    }
+
+    return result;
+}
+
+vector<vector<int>> composition(vector<vector<int>> a, vector<vector<int>> b)
+{
+    vector<vector<int>> result;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        for (int j = 0; j < b.size(); j++)
+        {
+            if (a[i][1] == b[j][0])
+                result.push_back({ a[i][0], b[j][1] });
+        }
+    }
+
+    return sort(result);
+}
+
+vector<vector<int>> dek_multiplication(vector<int> a, vector<int> b)
+{
+    vector<vector<int>> result;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        for (int j = 0; j < b.size(); j++)
+        {
+            result.push_back({ a[i], b[j] });
+        }
+    }
+
+    return sort(result);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    //Вектор для промежуточных значений
+    vector<int> projection;
+    vector<vector<int>> graf;
+    //Переменные
+    int P_length;
+    int Q_length;
+    int choice;
+    int buf1, buf2;
+
+    vector<vector<int>> P;
+    vector<vector<int>> Q;
+
+    cout << "Как вы хотите ввести графики?" << endl;
+    cout << "1. Вручную." << endl << "2. Высказыванием." << endl;
+    do {
+        cout << "Ваш выбор: ";
+        cin >> choice;
+    } while (choice < 1 || choice > 2);
+
+    if (choice == 1)
+    {
+        cout << "Введите мощность графика P: ";
+        cin >> P_length;
+
+
+        cout << "Вводите график P: " << endl;
+        for (int i = 0; i < P_length; i++)
+        {
+             cin >> buf1;
+             cin >> buf2;
+
+             P.push_back({ buf1, buf2 });
+        }
+        cout << "-----------------------------------------------------------------------" << endl;
+
+        while (P != sort(P))
+        {
+            cout << "Ошибка ввода! Проверьте правильность введенных данных (исключите повторяющиеся пары) и повторите ввод." << endl;
+            cout << "Вводите график P: " << endl;
+            for (int i = 0; i < P_length; i++)
+                {
+                    cin >> buf1;
+                    cin >> buf2;
+
+                    P.push_back({ buf1, buf2 });
+                }
+            cout << "-----------------------------------------------------------------------" << endl;
+        }
+
+        cout << "Ваш график P:" << endl;
+        gr_output(P);
+
+        cout << "Введите мощность графика Q: ";
+        cin >> Q_length;
+
+        cout << "Вводите график Q: " << endl;
+        for (int i = 0; i < Q_length; i++)
+            {
+                cin >> buf1;
+                cin >> buf2;
+
+                Q.push_back({ buf1, buf2 });
+            }
+        cout << "-----------------------------------------------------------------------" << endl;
+
+        while (Q != sort(Q))
+        {
+            cout << "Ошибка ввода! Проверьте правильность введенных данных (исключите повторяющиеся пары) и повторите ввод." << endl;
+            cout << "Вводите график Q: " << endl;
+            for (int i = 0; i < Q_length; i++)
+                {
+                    cin >> buf1;
+                    cin >> buf2;
+
+                    Q.push_back({ buf1, buf2 });
+                }
+            cout << "-----------------------------------------------------------------------" << endl;
+        }
+
+        cout << "Ваш график Q:" << endl;
+        gr_output(Q);
+    }
+    else
+    {
+        cout << "Будет использовано высказывание x = n, y = 2n." << endl;
+        cout << "Введите мощность графика P: ";
+        cin >> P_length;
+
+
+        cout << "Введите мощность графика Q: ";
+        cin >> Q_length;
+
+
+        for (int n = 0; n < P_length; n++)
+        {
+            P.push_back({ n, (2*n) });
+        }
+
+        for (int n = 0; n < Q_length; n++)
+        {
+            Q.push_back({ n, (2*n) });
+        }
+
+        cout << "Ваш график P:" << endl;
+        gr_output(P);
+
+        cout << "Ваш график Q:" << endl;
+        gr_output(Q);
+    }
+//ОПЕРАЦИИ:
+
+    //ОБЛАСТЬ ОПРЕДЕЛЕНИЯ:
+
+    cout << "Область определения P (проекция на ось Ox):" << endl;
+    
+    projection = proj_on_axis(P, 1);
+    proj_output(projection);
+
+    cout << "Область определения Q (проекция на ось Ox):" << endl;
+
+    projection = proj_on_axis(Q, 1);
+    proj_output(projection);
+
+    //МНОЖЕСТВО ЗНАЧЕНИЙ:
+
+    cout << "Множество значений P (проекция на ось Oy):" << endl;
+
+    projection = proj_on_axis(P, 2);
+    proj_output(projection);
+
+    cout << "Множество значений Q (проекция на ось Oy):" << endl;
+
+    projection = proj_on_axis(Q, 2);
+    proj_output(projection);
+
+    //ИНВЕРСИЯ:
+
+    cout << "Инверсия графика P: " << endl;
+
+    graf = inversion(P);
+    gr_output(graf);
+
+    cout << "Инверсия графика Q: " << endl;
+
+    graf = inversion(Q);
+    gr_output(graf);
+
+    //КОМПОЗИЦИЯ:
+
+    cout << "Композиция графиков P·Q:" << endl;
+
+    graf = composition(P, Q);
+    gr_output(graf);
+
+    cout << "Композиция графиков Q·P:" << endl;
+    
+    graf = composition(Q, P);
+    gr_output(graf);
+
+    //ДЕКАРТОВО ПРОИЗВЕДЕНИЕ:
+
+    cout << "Декартово произведение проекций графиков P и Q на Ox:" << endl;
+    
+    graf = dek_multiplication( proj_on_axis(P, 1), proj_on_axis(Q, 1) );
+    gr_output(graf);
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ tmois lab3/tmois lab3/tmois lab3.cpp
@@ -0,0 +1,519 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <locale>
+using namespace std;
+void Sort(vector<int>& arr)
+{
+    vector<int> result; // Вектор для хранения уникальных элементов
+
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        bool isUnique = true; // Флаг для проверки уникальности элемента
+        // Проверяем, существует ли элемент в векторе уникальных элементов
+        for (int j = 0; j < result.size(); ++j)
+        {
+            if (arr[i] == result[j])
+            {
+                isUnique = false; // Элемент не уникален
+                break;            // Прерываем цикл, если нашли дубликат
+            }
+        }
+        // Если элемент уникальный, добавляем его в вектор уникальных элементов
+        if (isUnique)
+        {
+            result.push_back(arr[i]);
+        }
+    }
+
+    // Очищаем исходный вектор и заполняем его уникальными элементами
+    arr.clear();
+    arr = result;
+}
+void Sort(vector<pair<int, int>>& arr)
+{
+    vector<pair<int, int>> result;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        bool flag = true;
+        for (int j = 0; j < result.size(); j++)
+        {
+            if (arr[i].first == result[j].first && arr[i].second == result[j].second)
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+        {
+            result.push_back(make_pair(arr[i].first, arr[i].second));
+        }
+    }
+    arr.clear();
+    arr = result;
+}
+vector<int> Union(vector<int> a, vector<int> b)
+{
+    vector<int> result;
+    result = a;
+    for (int i = 0; i < b.size(); i++)
+    {
+        result.push_back(b[i]);
+    }
+    Sort(result);
+    return result;
+}
+vector<pair<int, int>> Union(vector<pair<int, int>> a, vector<pair<int, int>> b)
+{
+    vector<pair<int, int>> result;
+    result = a;
+    for (int i = 0; i < b.size(); i++)
+    {
+        result.push_back(b[i]);
+    }
+    Sort(result);
+    return result;
+}
+vector<int> Intersection(vector<int> a, vector<int> b)
+{
+    vector<int> result;
+    for (int i = 0; i < a.size(); i++)
+    {
+        bool isSimple = false;
+        for (int j = 0; j < b.size(); j++)
+            if (a[i] == b[j])
+            {
+                isSimple = true;
+                break;
+            }
+        if (isSimple)
+        {
+            result.push_back(a[i]);
+        }
+    }
+    Sort(result);
+    return result;
+}
+vector<pair<int, int>> Intersection(vector<pair<int, int>> a, vector<pair<int, int>> b)
+{
+    vector<pair<int, int>> result;
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        bool flag = false;
+        for (int j = 0; j < b.size(); j++)
+        {
+            if (a[i].first == b[j].first && a[i].second == b[j].second)
+            {
+                flag = true;
+                break;
+            }
+        }
+        if (flag)
+        {
+            result.push_back(a[i]);
+        }
+    }
+    Sort(result);
+    return result;
+}
+//vector<int> Residual(vector<int> a, vector<int> b)
+//{
+//    vector<int> result;
+//    vector<int> intersected = Intersection(a, b);
+//    for (int i = 0; i < a.size(); i++)
+//    {
+//        bool isSimple = true;
+//        for (int j = 0; j < intersected.size(); j++)
+//        {
+//            if (a[i] == intersected[j])
+//            {
+//                isSimple = false;
+//                break;
+//            }
+//        }
+//        if (isSimple)
+//        {
+//            result.push_back(a[i]);
+//        }
+//    }
+//    Sort(result);
+//    return result;
+//}
+
+vector<int> Residual(vector<int> a, vector<int> b)
+{
+    vector<int> result;
+    bool contain; //флаг вхождения
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        contain = false;
+        for (int j = 0; j < b.size(); j++)
+        {
+            if (a[i] == b[j]) //условие, при котором a[i] не добавляется в результирующее множество
+            {
+                contain = true;
+                break;
+            }
+        }
+
+        if (contain == false) result.push_back(a[i]); //условие, при котором а[i] добавляется в результирующее множество
+    }
+
+    return result;
+}
+vector<pair<int, int>> Residual(vector<pair<int, int>> a, vector<pair<int, int>> b)
+{
+    vector<pair<int, int>> result;
+    bool contain;
+
+    /*vector<pair<int, int>> intersected = Intersection(a, b);*/
+
+    for (int i = 0; i < a.size(); i++)
+    {
+        bool contain = false;
+        for (int j = 0; j < b.size(); j++)
+        {
+            if (a[i].first == b[j].first && a[i].second == b[j].second)
+            {
+                contain = true;
+                break;
+            }
+        }
+        if (contain == false)
+        {
+            result.push_back(a[i]);
+        }
+    }
+    /*Sort(result);*/
+    return result;
+}
+vector<pair<int, int>> Composition(vector<pair<int, int>> a, vector<pair<int, int>> b)
+{
+    vector<pair<int, int>> result;
+    for (int i = 0; i < a.size(); i++)
+    {
+        for (int j = 0; j < b.size(); j++)
+        {
+            if (a[i].second == b[j].first)
+            {
+                result.push_back(make_pair(a[i].first, b[j].second));
+            }
+        }
+    }
+    Sort(result);
+    return result;
+}
+vector<pair<int, int>> inversion(vector<pair<int, int>> a)
+{
+    vector<pair<int, int>> result;
+    for (int i = 0; i < a.size(); i++)
+    {
+        result.push_back(make_pair(a[i].second, a[i].first));
+    }
+    return result;
+}
+void vivod(vector<int> a)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        cout << a[i] << ' ';
+    }
+    cout << endl;
+}
+
+class Combination
+{
+private:
+    vector<int> X;
+    vector<int> Y;
+    vector<pair<int, int>> G;
+
+public:
+    vector<pair<int, int>> getG()
+    {
+        return G;
+    }
+
+    void enumereation()
+    {
+        int sizeX, sizeY, sizeG;
+        cout << "enter size of X" << endl;
+        while (!(cin >> sizeX) || cin.get() != '\n')
+        {
+            cout << "Error!" << endl
+                << "Введите числовое значение : ";
+            cin.clear();
+            while (cin.get() != '\n')
+                ;
+        }
+
+        cout << "enter size of Y" << endl;
+        while (!(cin >> sizeY) || cin.get() != '\n')
+        {
+            cout << "Error!" << endl
+                << "Введите числовое значение : ";
+            cin.clear();
+            while (cin.get() != '\n')
+                ;
+        }
+        cout << "enter " << sizeX << " elements of X" << endl;
+        for (int i = 0; i < sizeX; i++)
+        {
+            int temp;
+            while (!(cin >> temp) || cin.get() != '\n')
+            {
+                cout << "Error!" << endl
+                    << "Введите числовое значение : ";
+                cin.clear();
+                while (cin.get() != '\n')
+                    ;
+            }
+            X.push_back(temp);
+        }
+        cout << "enter " << sizeY << " elements of Y " << endl;
+        for (int i = 0; i < sizeY; i++)
+        {
+            int temp;
+            while (!(cin >> temp) || cin.get() != '\n')
+            {
+                cout << "Error!" << endl
+                    << "Введите числовое значение : ";
+                cin.clear();
+                while (cin.get() != '\n')
+                    ;
+            }
+            Y.push_back(temp);
+        }
+        cout << "enter number of combinations" << endl;
+        cin >> sizeG;
+        cout << "enter " << sizeG << " elements of G (pairs)" << endl;
+        for (int i = 0; i < sizeG; i++)
+        {
+            int temp1;
+            int temp2;
+            cin >> temp1 >> temp2;
+            G.push_back(make_pair(temp1, temp2));
+        }
+    }
+    void constant()
+    {
+        int sizeX, sizeY, sizeG;
+        cout << "enter size of X" << endl;
+        while (!(cin >> sizeX) || cin.get() != '\n')
+        {
+            cout << "Error!" << endl
+                << "Введите числовое значение : ";
+            cin.clear();
+            while (cin.get() != '\n')
+                ;
+        }
+
+        cout << "enter size of Y" << endl;
+        while (!(cin >> sizeY) || cin.get() != '\n')
+        {
+            cout << "Error!" << endl
+                << "Введите числовое значение : ";
+            cin.clear();
+            while (cin.get() != '\n')
+                ;
+        }
+
+        cout << "enter x for formula: x+i for X and x+i+1 for Y" << endl;
+        int x;
+        while (!(cin >> x) || cin.get() != '\n')
+        {
+            cout << "Error!" << endl
+                << "Введите числовое значение : ";
+            cin.clear();
+            while (cin.get() != '\n')
+                ;
+        }
+
+        for (int i = 0; i < sizeX; i++)
+        {
+            X.push_back(x + i);
+        }
+
+        for (int i = 0; i < sizeX; i++)
+        {
+            Y.push_back(x + i + 1);
+        }
+        cout << "enter number of combinations" << endl;
+        cin >> sizeG;
+        cout << "combinations will be picked in this formula: 1-1 1-2 1-3...x-y";
+        for (int i = 0; i < sizeG / sizeY + 1; i++)
+        {
+            for (int j = 0; j < sizeY; j++)
+            {
+                if (G.size() == sizeG)
+                    return;
+                G.push_back(make_pair(X[i], Y[j]));
+            }
+        }
+    }
+    void show()
+    {
+        cout << "\nX\n";
+        for (int i = 0; i < X.size(); i++)
+        {
+            cout << X[i] << ' ';
+        }
+        cout << "\nY\n";
+        for (int i = 0; i < Y.size(); i++)
+        {
+            cout << Y[i] << ' ';
+        }
+        cout << "\nG\n";
+        for (int i = 0; i < G.size(); i++)
+        {
+            cout << G[i].first << " ," << G[i].second << endl;
+        }
+    }
+    Combination operator+(const Combination c2) const
+    {
+        Combination res;
+        res.X = Union(X, c2.X);
+        res.Y = Union(Y, c2.Y);
+        res.G = Union(G, c2.G);
+        return res;
+    }
+    Combination operator-(const Combination c2) const
+    {
+        Combination res;
+        res.X = Intersection(X, c2.X);
+        res.G = Intersection(G, c2.G);
+        res.Y = Intersection(Y, c2.Y);
+        return res;
+    }
+    Combination operator/(const Combination c2) const
+    {
+        Combination res;
+        res.X = Residual(X, c2.X);
+        res.Y = Residual(Y, c2.Y);
+
+        vector<pair<int, int>> temp;
+        temp = Residual(G, c2.G);
+
+        bool contain_X = false;
+        bool contain_Y = false;
+
+        for (int i = 0; i < temp.size(); i++)
+        {
+            
+            contain_X = false;
+            contain_Y = false;
+
+            for (int j = 0; j < res.X.size(); j++)
+            {
+                if (temp[i].first == res.X[j])
+                {
+                    contain_X = true;
+                }
+            }
+            
+            for (int k = 0; k < res.Y.size(); k++)
+            {
+                if (temp[i].second == res.Y[k])
+                {
+                    contain_Y = true;
+                }
+            }
+
+            if ((contain_Y == true) && (contain_X == true))
+            {
+                res.G.push_back(temp[i]);
+
+            }
+        }
+
+        return res;
+    }
+    Combination Inversion()
+    {
+        Combination res;
+        res.X = Y;
+        res.Y = X;
+        res.G = inversion(G);
+        return res;
+    }
+    Combination operator*(const Combination c2) const
+    {
+        Combination res;
+        res.X = X;
+        res.Y = c2.Y;
+        res.G = Composition(G, c2.G);
+        return res;
+    }
+};
+vector<int> view(Combination c, vector<int> a, int oper)
+{
+    vector<int> result;
+    vector<pair<int, int>> g = c.getG();
+    if (oper == 1)
+    {
+        for (int i = 0; i < a.size(); i++)
+        {
+            for (int j = 0; j < g.size(); j++)
+            {
+                if (a[i] == g[j].first)
+                {
+                    result.push_back(g[j].second);
+                }
+            }
+        }
+        Sort(result);
+        return result;
+    }
+    else
+    {
+        for (int i = 0; i < a.size(); i++)
+        {
+            for (int j = 0; j < g.size(); j++)
+            {
+                if (a[i] == g[j].second)
+                {
+                    result.push_back(g[j].first);
+                }
+            }
+        }
+        Sort(result);
+        return result;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    vector<int> a = { 1, 2, 3, 4 };
+    Combination c1;
+    c1.constant();
+    c1.show();
+    Combination c2;
+    c2.constant();
+    c2.show();
+    cout << "\nunion: \n";
+    (c1 + c2).show();
+    cout << "\nintersection: \n";
+    (c1 - c2).show();
+    cout << "\nresidual: \n";
+    (c1 / c2).show();
+    cout << endl;
+    (c2 / c1).show();
+    cout << "\ncomposition:\n";
+    (c1 * c2).show();
+    cout << "\ninversion:\n";
+    (c1.Inversion()).show();
+    cout << endl;
+    (c2.Inversion()).show();
+    cout << "view\n";
+    vivod(view(c1, a, 1));
+    cout << endl;
+    vivod(view(c2, a, 1));
+    cout << "another view\n";
+    vivod(view(c1, a, 0));
+    cout << endl;
+    vivod(view(c2, a, 0));
+}
